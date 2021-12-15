811de65560d763173f4bf677c1e318faa521e81c
Krasimir Koev <krasimir.koev@bedegaming.com>
2021-12-15 13:01:07 +0200

    wip: some devastating code

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
 import json
+from src.lib.apiResponse import apiResponse
+from src.lib.db import db
+from src.api import api
+import uvicorn
 
-import requests
-import logging
 
-# Init Logger
-log = logging.getLogger("MAIN")
-logging.basicConfig()
-log.setLevel(logging.INFO)
 
-# Base URL and necessary vars
-baseurl = "https://prices.azure.com/api/retail/prices?$filter=endswith(meterName, 'Spot') and armRegionName eq 'northeurope'&$skip="
-keepPolling = True
-dataSet = []
-batch = 0
+configFile = open('./config.json')
+configData = json.load(configFile)
 
-# Work around for MS only returning 100 results at a time
-while(keepPolling == True):
-    url = baseurl + str(batch)
-    log.info(url)
-    result = requests.get(url).json()
-    dataSet.append(result)
 
-    # Loop until there is no longer an extra 100 results remaining
-    if(result['Count'] != 100):
-        keepPolling = False
+from src.lib.db import db
+db = db.DB(configData)
 
-    # Increment in batches of 100
-    batch += 100
+api(db, config)
 
-# Write dataset to file (for debugging)
-with open('dataset.json', 'w') as f:
-    json.dump(dataSet, f)
+# bla = db.dbClient.bla.blaCol.find({"hello": "world"})
 
-# Results now usable within the dataset object, this is necessary as we need to convert the array of combined API calls back to one JSON object
-dataSet = json.dumps(dataSet)
-dataset = json.loads(dataSet)
+uvicorn.run("app.api:app", host="0.0.0.0", port=8080, reload=True)
+
